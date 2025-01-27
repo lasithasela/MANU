89db816ac8abd0dec894c77427dcfe14d5ce0d45
lasithasela <161576715+lasithasela@users.noreply.github.com>
2025-01-27 09:16:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
-GITHUB_USERNAME: process.env.GITHUB_USERNAME || "put your github usearname",
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "put your github auth token without ghp_",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&KcMigLSQ#707zNOJqnZ2iPwt182n2WzIQWnnmUUwAHr9Hy-jweGI",
+GITHUB_USERNAME: process.env.GITHUB_USERNAME || "lasithasela",
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "KFjNLxSJurE48DEA5fOzocvc49LeGF0dXsEJ",
 };
